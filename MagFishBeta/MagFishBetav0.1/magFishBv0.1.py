8dcd41f856408e69280c3819c635a686bc1cb82a
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2016-11-04 16:34:59 +0000

    reordered main script to account for argument parsing

--- MagFishBeta/MagFishBetav0.1/magFishBv0.1.py
+++ MagFishBeta/MagFishBetav0.1/magFishBv0.1.py
@@ -1,17 +1,8 @@
-print("\nInitialising...")
-print("""
- _____         _____ _     _
-|     |___ ___|   __|_|___| |_
-| | | | .'| . |   __| |_ -|   |
-|_|_|_|__,|_  |__|  |_|___|_|_|
-          |___|
-""")
-
 print("Importing required packages...",end="")
 try:
     import argparse #Used for parsing command line arguments
-    import ValidateIP #Function for validating whtether a string is a valid IPv4 or IPv6 address
     import ipaddress
+    import RequestURL
     print("Done")
 except ImportError as error:
     print("There was an error importing one or more of the modules. Are they named correctly?")
@@ -22,10 +13,18 @@ except Exception as error:
     print("\nError: " + str(error))
     sys.exit()
 
-print("Parsing arguments...",end="")
-
 parser = argparse.ArgumentParser()
 parser.add_argument("ip",help="The IP address of the server.",type=ipaddress.IPv4Address)
 args = parser.parse_args()
 
-print("Done")
+print("\nInitialising...")
+print("""
+ _____         _____ _     _
+|     |___ ___|   __|_|___| |_
+| | | | .'| . |   __| |_ -|   |
+|_|_|_|__,|_  |__|  |_|___|_|_|
+          |___|
+""")
+
+
+print(RequestURL.get("/redfish/v1",False))
