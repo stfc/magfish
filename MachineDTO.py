f424aa42c9b9244a0ed47bccd929ddd231cfc1e5
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-04-06 16:09:19 +0100

    Copied machine class for DTO

---
+++ MachineDTO.py
@@ -0,0 +1,71 @@
+class Machine:
+
+    ip = ""
+    mac = ""
+
+    fqdn = ""
+    vendor = ""
+    manufact = ""
+
+    rack = -1
+    position = -1
+    units = -1
+
+    def __init__ (self,ip):
+        self.ip = ip
+
+    def getIp(self):
+        return self.ip
+    def setIp(self,ip):
+        self.ip = ip
+    def displayIp(self):
+        print("\nIP Address:" + self.getIp())
+
+    def getMac(self):
+        return self.mac
+    def setMac(self,mac):
+        self.mac = mac
+    def displayIp(self):
+        print("\nMAC Address:" + self.getMac())
+
+    def getFqdn(self):
+        return self.fqdn
+    def setFqdn(self,fqdn):
+        self.fqdn = fqdn
+    def displayFqdn(self):
+        print("\nFQDN:" + self.getFqdn())
+
+    def getVendor(self):
+        return self.vendor
+    def setVendor(self,vendor):
+        self.vendor = vendor
+    def displayVendor(self):
+        print("\nVendor:" + self.getVendor())
+
+    def getManufact(self):
+        return self.manufact
+    def setManufact(self,manufact):
+        self.manufact = manufact
+    def displayManufact(self):
+        print("\nManufacturer:" + self.getManufact())
+
+    def getRack(self):
+        return self.rack
+    def setRack(self,rack):
+        self.rack = rack
+    def displayRack(self):
+        print("\nServer Rack ID:" + self.getRack())
+
+    def getPosition(self):
+        return self.position
+    def setPosition(self,position):
+        self.position = position
+    def displayPosition(self):
+        print("\nPosition of Machine in Rack:" + self.getPosition())
+
+    def getUnits(self):
+        return self.Units
+    def setUnits(self,Units):
+        self.Units = Units
+    def displayUnits(self):
+        print("\nHeight of Machine in Units:" + self.getUnits())
