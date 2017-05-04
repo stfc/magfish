cdc05db576e7643e13b8c449e177baa830de7441
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-05-04 13:17:32 +0100

    Removed from machine

--- Machine.py
+++ Machine.py
@@ -19,86 +19,38 @@ class Machine:
     def __init__ (self,ip):
         self.ip = ip
 
-    def getIp(self):
-        return self.ip
-    def setIp(self,ip):
-        self.ip = ip
     def displayIp(self):
         print("\nIP Address:" + self.getIp())
 
-    def getMac(self):
-        return self.mac
-    def setMac(self,mac):
-        self.mac = mac
     def displayIp(self):
         print("\nMAC Address:" + self.getMac())
 
-    def getFqdn(self):
-        return self.fqdn
-    def setFqdn(self,fqdn):
-        self.fqdn = fqdn
     def displayFqdn(self):
         print("\nFQDN:" + self.getFqdn())
 
-    def getManufact(self):
-        return self.manufact
-    def setManufact(self,manufact):
-        self.manufact = manufact
     def displayManufact(self):
         print("\nManufacturer:" + self.getManufact(self))
 
-    def getModel(self):
-        return self.model
-    def setModel(self,model):
-        self.model = model
     def displayModel(self):
         print("\nModel:" + self.getModel())
 
-    def getSerialNo(self):
-        return self.serialNo
-    def setSerialNo(self,serialNo):
-        self.serialNo = serialNo
     def displaySerialNo(self):
         print("\nSerial Number:" + self.getSerialNo())
 
-    def getCoreCount(self):
-        return self.coreCount
-    def setCoreCount(self,coreCount):
-        self.coreCount = coreCount
     def displayCoreCount(self):
         print("\nCore Count:" + str(coreCount))
 
-    def getCoreModel(self):
-        return self.coreModel
-    def setCoreModel(self,coreModel):
-        self.coreModel = coreModel
     def displayCoreModel(self):
         print("\nSerial Number:" + self.getCoreModel())
 
-    def getTotalMemory(self):
-        return self.totalMemory
-    def setTotalMemory(self,totalMemory):
-        self.totalMemory = totalMemory
     def displayTotalMemory(self):
         print("\nTotal System Memory" + str(totalMemory) + "GB")
 
-    def getRack(self):
-        return self.rack
-    def setRack(self,rack):
-        self.rack = rack
     def displayRack(self):
         print("\nServer Rack ID:" + self.getRack())
 
-    def getPosition(self):
-        return self.position
-    def setPosition(self,position):
-        self.position = position
     def displayPosition(self):
         print("\nPosition of Machine in Rack:" + self.getPosition())
 
-    def getUnits(self):
-        return self.Units
-    def setUnits(self,Units):
-        self.Units = Units
     def displayUnits(self):
         print("\nHeight of Machine in Units:" + self.getUnits())
