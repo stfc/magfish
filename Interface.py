d2930631336af1a9f02b74fff81e6e0bd37eff08
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-05-05 10:34:52 +0100

    Added status parameter and changed functions

--- Interface.py
+++ Interface.py
@@ -3,11 +3,13 @@ class Interface:
     id = -1
     mac = ""
     active = False;
+    status = ""
 
-    def __init__ (self,id,mac,active):
+    def __init__ (self,id,mac,active,status):
         self.id = id
         self.mac = mac;
         self.active = active;
+        self.status = status;
 
     def displayId(self):
         print("\nID: " + str(self.id))
@@ -15,13 +17,13 @@ class Interface:
     def displayMac(self):
         print("\nMAC Address: " + self.mac)
 
-    def displayActive(self):
-        print("\nActive: " + str(self.active))
+    def displayStatus(self):
+        print("\Status: " + str(self.status))
 
     def displayAll(self):
         print("\nInterface:")
         print("================")
         self.displayId()
         self.displayMac()
-        self.displayActive()
+        self.displayStatus()
         print("================")
